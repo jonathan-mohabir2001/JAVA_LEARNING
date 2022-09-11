6a0848fbbb7a1d92aa2d3d5df065129eaba4e98c
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-09-11 17:52:45 -0400

    first java code added

---
+++ Java_Code/firstLineOfJava/src/Main.java
@@ -0,0 +1,18 @@
+
+/**
+ * Here is my first lines of java programming. 
+ */
+public class Main {
+
+
+    
+    public static void main(String[] args) {
+    
+        System.out.println("My name is Jonathan and here is lines of Java");
+        
+  
+    }
+
+ 
+    
+}
